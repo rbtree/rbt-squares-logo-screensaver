6a6d2660a62fbb77c125157da01485b018dd9b3c
Srđan Marković <srdjan.markovic@rbt.rs>
2019-11-12 13:36:50 +0100

    Experiment and improve boxes rotation and translation

--- RBT Squares Logo/RBT Squares Logo Screen Saver/RBTSquaresLogoScreenSaverView.swift
+++ RBT Squares Logo/RBT Squares Logo Screen Saver/RBTSquaresLogoScreenSaverView.swift
@@ -65,23 +65,46 @@ class RBTSquaresLogoScreenSaverView: ScreenSaverView {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ]
         
+        //static let rbtLogoPulse: [[Float]] = [
+        //    [0,32,48,56,60,62,63,64,64,63,62,60,56,48,32,0],
+        //    [32,64,80,88,92,94,95,96,96,95,94,92,88,80,64,32],
+        //    [48,80,96,104,108,110,111,112,112,111,110,108,104,96,80,48],
+        //    [56,88,104,112,116,118,119,120,120,119,118,116,112,104,88,56],
+        //    [60,92,108,116,120,122,123,124,124,123,122,120,116,108,92,60],
+        //    [62,94,110,118,122,124,125,126,126,125,124,122,118,110,94,62],
+        //    [63,95,111,119,123,125,126,127,127,126,125,123,119,111,95,63],
+        //    [64,96,112,120,124,126,127,128,128,127,126,124,120,112,96,64],
+        //    [64,96,112,120,124,126,127,128,128,127,126,124,120,112,96,64],
+        //    [63,95,111,119,123,125,126,127,127,126,125,123,119,111,95,63],
+        //    [62,94,110,118,122,124,125,126,126,125,124,122,118,110,94,62],
+        //    [60,92,108,116,120,122,123,124,124,123,122,120,116,108,92,60],
+        //    [56,88,104,112,116,118,119,120,120,119,118,116,112,104,88,56],
+        //    [48,80,96,104,108,110,111,112,112,111,110,108,104,96,80,48],
+        //    [32,64,80,88,92,94,95,96,96,95,94,92,88,80,64,32],
+        //    [0,32,48,56,60,62,63,64,64,63,62,60,56,48,32,0]
+        //]
+        
         static let boxSize = CGFloat(2.0)
         
         static var boxTypes: [SCNBox] {
+            let clearBox = SCNBox(width: boxSize, height: boxSize, length: boxSize, chamferRadius: 0.05)
+            clearBox.firstMaterial?.diffuse.contents = self.rbtWhite.withAlphaComponent(0.05)
+            clearBox.firstMaterial?.specular.contents = self.rbtWhite
+            
             let whiteBox = SCNBox(width: boxSize, height: boxSize, length: boxSize, chamferRadius: 0.05)
             whiteBox.firstMaterial?.diffuse.contents = self.rbtWhite
             whiteBox.firstMaterial?.specular.contents = self.rbtWhite
             
             let blackBox = SCNBox(width: boxSize, height: boxSize, length: boxSize, chamferRadius: 0.05)
             blackBox.firstMaterial?.diffuse.contents = self.rbtBlack
-            blackBox.firstMaterial?.specular.contents = self.rbtWhite.withAlphaComponent(0.25)
+            blackBox.firstMaterial?.specular.contents = self.rbtWhite
             
             let redBox = SCNBox(width: boxSize, height: boxSize, length: boxSize, chamferRadius: 0.05)
             redBox.firstMaterial?.diffuse.contents = self.rbtRed
-            redBox.firstMaterial?.specular.contents = self.rbtWhite.withAlphaComponent(0.25)
+            redBox.firstMaterial?.specular.contents = self.rbtWhite
 
             return [
-                SCNBox(),
+                clearBox,
                 redBox,
                 blackBox,
                 whiteBox
@@ -101,33 +124,54 @@ class RBTSquaresLogoScreenSaverView: ScreenSaverView {
             let bd = 1.0 / bps /* beat duration */
             
             // index offset
-            let off = Float(self.rbtLogo.count - 1) / 2.0
+            let off = Double(self.rbtLogo.count - 1) / 2.0
             
+            // max rotation angle
+            let srot = Double(.pi / 72.0)
+
             for (j, row) in self.rbtLogo.enumerated() {
                 //var rowVals = [Any]()
                 for (i, value) in row.enumerated() {
                     // offset index in a matrix
-                    let ox = Float(i) - off /* -off to +off, depending to a position in a row */
-                    let oy = off - Float(j) /* +off to -off, depending to a position in a col */
+                    let ox = Double(i) - off /* -off to +off, depending to a position in a row */
+                    let oy = off - Double(j) /* +off to -off, depending to a position in a col */
 
                     // absolute positions per offset index in a matrix
                     let xpos = self.boxSize * CGFloat(ox) /* -off * boxSize to +off * boxSize, depending on a position in a row */
                     let ypos = self.boxSize * CGFloat(oy) /* -off * boxSize to +off * boxSize, depending on a position in a col */
                     let zpos = CGFloat(0.0) /* 0.0 */
-                    
-                    // absolute transitions per offset index in a matrix
+                                        
+                    // absolute x and y transitions based on the offset index in a matrix
                     let xtrans = self.boxSize * CGFloat(ox / off) /* -boxSize to 0 to +boxSize, depending to a position in a row, more at the edges */
                     let ytrans = self.boxSize * CGFloat(oy / off) /* -boxSize to 0 to +boxSize, depending to a position in a col, more at the edges */
-                    let ztrans = self.boxSize * CGFloat(1.5 * (off - max(abs(ox), abs(oy))) / off) /* 0 to +boxSize to 0, depending to a position in a matrix, more at the center */
+                    
+                    // absolute z transition based on the offset index in a matrix
+                    //let ztrans = self.boxSize * CGFloat(1.5 * (off - max(Swift.abs(ox), Swift.abs(oy))) / off) /* 0 to +boxSize to 0, depending to a position in a matrix, more at the center */
+                    
+                    // absolute z transition based on the logarithm
+                    let ztrans = self.boxSize * CGFloat(simd.log((off - Swift.abs(ox)) * (off - Swift.abs(oy))))
+                    
+                    // absolute z transition based on the color in the logo
+                    //let color = self.rbtLogo[j][i]
+                    //let ztrans = self.boxSize * CGFloat(Float(color) / 2.0)
+                    
+                    // absolute z transition based on the predefined pulse matrix
+                    //let pulse = self.rbtLogoPulse[j][i] / 128.0
+                    //let ztrans = self.boxSize * CGFloat(pulse * 2.0)
+                    
+                    //rowVals.append(String(format: "%.3f", ztrans))
 
-                    // absolute rotations per offset index in a matrix
-                    let srot = CGFloat(.pi / 72.0)
-                    let xrot = srot * CGFloat(-sign(oy) * (off - max(abs(ox), abs(oy))) / off)
-                    let yrot = srot * CGFloat(sign(ox) * (off - max(abs(ox), abs(oy))) / off)
+                    // absolute rotations based on the offset index in a matrix
+                    //let xrot = CGFloat(srot) * CGFloat(-sign(oy) * (off - max(Swift.abs(ox), Swift.abs(oy))) / off)
+                    //let yrot = CGFloat(srot) * CGFloat(sign(ox) * (off - max(Swift.abs(ox), Swift.abs(oy))) / off)
+                    //let zrot = CGFloat(0.0)
+
+                    // absolute rotations based on the logarithm
+                    let xrot = CGFloat(srot) * CGFloat(-sign(oy) * (log((Swift.abs(ox) + 1.0) * (Swift.abs(oy) + 1.0))))
+                    let yrot = CGFloat(srot) * CGFloat(sign(ox) * (log((Swift.abs(ox) + 1.0) * (Swift.abs(oy) + 1.0))))
                     let zrot = CGFloat(0.0)
 
-                    //rowVals.append(xtrans)
-
+                    // exclude empty boxes
                     guard value != 0 else { continue }
                     
                     let boxType = self.boxTypes[value]
@@ -218,16 +262,19 @@ class RBTSquaresLogoScreenSaverView: ScreenSaverView {
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
 
-        // place the camera
-        cameraNode.position = SCNVector3(32, 0, 32)
-        cameraNode.orientation = SCNQuaternion(0, .pi/8, 0, 1.0)
+        // place the camera directly above
+        //cameraNode.position = SCNVector3(0, 0, 32)
+        
+        // place the camera on the top-left
+        cameraNode.position = SCNVector3(16, -16, 32)
+        cameraNode.orientation = SCNQuaternion(.pi/16, .pi/16, 0, 1.0)
 
         // create and add a light to the scene
         let omniLightNode = SCNNode()
         omniLightNode.name = "omni"
         omniLightNode.light = SCNLight()
         omniLightNode.light?.type = .omni
-        omniLightNode.position = SCNVector3(x: 0, y: 64, z: 64)
+        omniLightNode.position = SCNVector3(x: -64, y: 32, z: 64)
         scene.rootNode.addChildNode(omniLightNode)
         
         // create and add an ambient light to the scene
